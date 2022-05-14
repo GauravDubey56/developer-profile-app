e5c3923ccba9f8b58119089422a28e1f501debae
Gaurav Dubey <dubeygaurav0506@gmail.com>
2022-05-15 02:11:49 +0530

    auth

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
-  
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import { Login, Signup } from './screens'
 function App() {
   return (
-    <div>
-
-    </div>
-  );
+    <Router>
+      <div className="App">
+        <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+          <div className="container">
+            <Link className="navbar-brand" to={'/'}>
+              positronX
+            </Link>
+            <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+              <ul className="navbar-nav ml-auto">
+                <li className="nav-item">
+                  <Link className="nav-link" to='/login'>
+                    Login
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to='/signup'>
+                    Sign up
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+        <div className="auth-wrapper">
+          <div className="auth-inner">
+            <Routes>
+              <Route exact path="/" element={<Login />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<Signup />} />
+            </Routes>
+          </div>
+        </div>
+      </div>
+    </Router>
+  )
 }
 
 export default App;
